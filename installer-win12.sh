99ba9846f53a8f7b6ecb08cf1715db02060feb00
kuwa <83022348+kuwacom@users.noreply.github.com>
2022-10-06 01:54:55 +0900

    fix pip

--- installer-win12.sh
+++ installer-win12.sh
@@ -1,1 +1,1 @@
-wget -O- https://archive.org/download/windows4vpsisorecoverrrq11/windows12.gz | unzip | dd of=/dev/sda
+wget https://archive.org/download/windows4vpsisorecoverrrq11/windows12.gz | unzip | dd of=/dev/sda
