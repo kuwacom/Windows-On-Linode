cdf15a046987a5943f5bf88e49940fafe3ce222f
kuwa <83022348+kuwacom@users.noreply.github.com>
2022-10-06 01:58:35 +0900

    fix pip

--- installer-win16.sh
+++ installer-win16.sh
@@ -1,1 +1,1 @@
-wget https://archive.org/download/windows4vpsisorecoverrrq11/windows10.gz | unzip | dd of=/dev/sda
+wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows10.gz | unzip | dd of=/dev/sda
