552b662b38841c726d0860d3098d163f76cdeb74
kuwa <83022348+kuwacom@users.noreply.github.com>
2022-10-18 18:59:46 +0900

    fix miss

--- installer-win16.sh
+++ installer-win16.sh
@@ -1,1 +1,1 @@
-wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows10.gz | unzip | dd of=/dev/sda
+wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows10.gz | gunzip | dd of=/dev/sda
