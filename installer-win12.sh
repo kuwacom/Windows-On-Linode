db97aa93a15ac391bec75493d10f59878d86d7b3
kuwa <83022348+kuwacom@users.noreply.github.com>
2022-10-18 18:59:14 +0900

    fix miss

--- installer-win12.sh
+++ installer-win12.sh
@@ -1,1 +1,1 @@
-wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows12.gz | unzip | dd of=/dev/sda
+wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows12.gz | gunzip | dd of=/dev/sda
