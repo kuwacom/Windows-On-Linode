12f39db91f21ed06b4fe901bac7dd5ee6f8379f5
kuwa <83022348+kuwacom@users.noreply.github.com>
2022-10-18 18:59:58 +0900

    fix miss

--- installer-win19.sh
+++ installer-win19.sh
@@ -1,1 +1,1 @@
-wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows19.gz | unzip | dd of=/dev/sda
+wget -O- --no-check-certificate https://archive.org/download/windows4vpsisorecoverrrq11/windows19.gz | gunzip | dd of=/dev/sda
